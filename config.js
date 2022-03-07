e76d9f3ba4ad6bc6e664fefe58183121a81ec4c1
nhoctienone1 <92415626+nhoctienone1@users.noreply.github.com>
2022-03-07 09:55:30 +0700

    upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Dì Thoa';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100009157854005';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng dì Thoa';
+const contentCard = 'Chúc dì thoa có một ngày 8/3 vui vẻ, hạnh phúc. Chúc dì ngày càng xinh đẹp, công việc thuận lợi, gặp nhiều may mắn';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
